a54ab162447e802155c47456a3e5efd57ea2f1ae
Exylus <thomy.erwan@gmail.com>
2022-06-14 02:00:36 +0200

    added port scanning method

--- scanner.py
+++ scanner.py
@@ -15,4 +15,17 @@ print("-" * 50)
 print("Scanning target "+target)
 print("Time started: "+str(time.now()))
 print("-" * 50)
+print("PORT SCANNER IS SCANNING ")
+print(time.now())
+
+
+try:
+    for port in range(1,65535):
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result=s.connect_ex((target,port))
+        print(port)
+        if result==0:
+            print("The %d is open"%(port))
+        s.close()
 
